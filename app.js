ea266a62aecb4140ab873b4b83125477076e0b65
DuhaQurashi <dqurashi1@sheffield.ac.uk>
2024-09-10 23:13:04 +0100

    week4-1

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-document.addEventListener("DOMContentLoaded", () =>{
-
-    const images = document.querySelectorAll("img");
+document.addEventListener('DOMContentLoaded', () => {
+    const OpenPopupLinkBlanche = document.getElementById('OpenPopupLinkBlanche');
+    const OpenPopupLinkKatt = document.getElementById('OpenPopupLinkKatt');
+    const OpenPopupLinkBlathers = document.getElementById('OpenPopupLinkBlathers');
+    const OpenPopupLinkTom = document.getElementById('OpenPopupLinkTom');
     
-    for(const image of images){
-        fetch("https://dog.ceo/api/breeds/image/random").then(response => response.json()).then(data => 
-            {
-                image.src = data.message
-                image.width = 100;
-                image.height =100;
-            })
+    OpenPopupLinkBlanche.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default anchor click behavior
+        openPopup('https://nookipedia.com/wiki/Blanche');
+    });
+    OpenPopupLinkKatt.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default anchor click behavior
+        openPopup('https://nookipedia.com/wiki/katt');
+    });
+    OpenPopupLinkBlathers.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default anchor click behavior
+        openPopup('https://nookipedia.com/wiki/Blathers');
+    });
+    OpenPopupLinkTom.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default anchor click behavior
+        openPopup('https://nookipedia.com/wiki/Tom_Nook');
+    });
+  
+    function openPopup(url) {
+        const width = 600;
+        const height = 600;
+        const left = (window.innerWidth / 2) - (width / 2);
+        const top = (window.innerHeight / 2) - (height / 2);
+        const features = `width=${width},height=${height},top=${top},left=${left},scrollbars=yes,resizable=yes`;
+  
+        window.open(url, 'popupWindow', features);
     }
-})
+  });
+  
